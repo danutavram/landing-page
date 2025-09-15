73872b3e1ca354510d3e42ef1c4ff6fc68ae670e
danutavram <avramnicolaedan@gmail.com>
2025-09-15 09:22:02 +0300

    components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Layout } from "./components/Layout"
+import { Hero } from "./components/sections/Hero"
+
 function App() {
 
   return (
-    <>
-    
-    </>
+    <Layout title="EdgeAI">
+      <Hero />
+    </Layout>
   )
 }
 
