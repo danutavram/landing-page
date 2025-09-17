b6e0a0744049c795e93fa5aadd02a338aa9c1a85
danutavram <avramnicolaedan@gmail.com>
2025-09-17 17:06:48 +0300

    Brands&Services created

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -6,10 +6,10 @@ interface ButtonProps {
     onClick?: ()=> void;
 }
 
-export const Button = ({className = "", children, onClick}: ButtonProps) =>{
+export const Button = ({onClick, children, className = ""}: ButtonProps) =>{
     return (
         <button onClick={onClick} className={`px-6 py-3 rounded-full outline-none cursor-pointer
-        relative overflow-hidden border border-transparent bg-violet-600`}>
+        relative overflow-hidden border border-transparent bg-violet-600 ${className}`}>
             {children}
         </button>
     )
