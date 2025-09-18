0b95ba4df72e4343e599e00040c4eac639d293af
danutavram <avramnicolaedan@gmail.com>
2025-09-18 11:19:04 +0300

    Footer completed

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -4,7 +4,7 @@ import { NavItem } from "../shared/NavItem";
 import { BtnLink } from "../shared/BtnLink";
 import { useThemeStore } from "../../store/ThemeStore";
 
-const navItems = [
+export const navItems = [
   { href: "#", text: "Home" },
   { href: "#services", text: "Services" },
   { href: "#about-us", text: "About Us" },
